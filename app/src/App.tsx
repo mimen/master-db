cbcf1a5184beaa89d726f0d3a7202a3bb93778ea
Milad Imen <mimen@salesforce.com>
2025-10-10 20:26:00 -0700

    feat: add toast notification system with sonner

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -2,6 +2,7 @@ import { ConvexProvider, ConvexReactClient } from "convex/react"
 
 import { DialogManager } from "@/components/dialogs/DialogManager"
 import { Layout } from "@/components/layout/Layout"
+import { Toaster } from "@/components/ui/sonner"
 import { DialogProvider } from "@/contexts/DialogContext"
 
 const convex = new ConvexReactClient(import.meta.env.VITE_CONVEX_URL as string)
@@ -12,6 +13,7 @@ export function App() {
       <DialogProvider>
         <Layout />
         <DialogManager />
+        <Toaster />
       </DialogProvider>
     </ConvexProvider>
   )
